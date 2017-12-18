cb4d8d310bd3f2e45e44d5c89b98cf0b02e372d6
Brent Guistwite <BrentGuistwite@gmail.com>
2017-12-18 18:43:55 -0500

    added second endpoint for madlib

--- server.js
+++ server.js
@@ -13,7 +13,20 @@ app.get('/echo/:what', (req, res) => {
   console.log(params);
 });
 
+app.get('/',( req, res ) => {
+  let results = req.query;
+  let sample = (`There's a ${results.adjective1} new ${results.name} in ${results.place} and everyone's talking. Stunningly 
+  ${results.adjective2} and ${results.adverb} ${results.adjective3}, all the cool kids know it. However, ${results.name} has
+   a secret - ${results.name}'s a vile vampire. 
+
+   Will it end with a bite, or with a stake through the bungee cord?`);
+
+  res.json(sample);
+  
+  
+});
 
 app.listen(8080, () => {
   console.log('Listening on port 8080');
 });
+
